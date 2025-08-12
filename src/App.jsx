eef1f114aea65e70ae88b44b14dfb68fcd9af614
12buHtiG <ryanjohnleary@gmail.com>
2025-08-12 18:31:30 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import './App.css';
+
+const Card = () => {
+	return <h1>BIGGER</h1>;
+};
+
+const App = () => {
+	return (
+		<>
+			<h1>Big</h1>
+			<h2>Small</h2>
+			<Card />
+		</>
+	);
+};
+
+export default App;
