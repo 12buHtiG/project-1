00d98d876d7bfb49a0e88dfd6b7dc299c745061a
12buHtiG <ryanjohnleary@gmail.com>
2025-08-19 17:55:49 +1000

    Updated the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import './App.css';
+// REMINDER - Take it LIVE! Publish it on the INTERWEBZ :)
 import './App.css';
 
-const Card = () => {
-	return <h1>BIGGER</h1>;
+const Card = ({ title }) => {
+	return (
+		<div className="card">
+			<h2>{title}</h2>
+		</div>
+	);
 };
 
 const App = () => {
 	return (
-		<>
-			<h1>Big</h1>
-			<h2>Small</h2>
-			<Card />
-		</>
+		<div className="card-container">
+			<Card title="Harry Fabricator" rating={8} isCool={true} />
+			<Card title="Unhappy Potter" rating={9} />
+			<Card title="Simple Jack" rating={4} />
+			<Card title="Mission Possible" rating={4} />
+			<Card title="Super Easy Mission" rating={4} />
+		</div>
 	);
 };
 
