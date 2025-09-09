f0c4ed56ea66fcbc56bfd76a624386f5c85a54b6
12buHtiG <ryanjohnleary@gmail.com>
2025-09-09 18:18:39 +1000

    implemented fancy fonts

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,21 @@ const Card = ({ title }) => {
 
 const App = () => {
 	return (
-		<div className="card-container">
-			<Card title="Harry Fabricator 123" rating={8} isCool={true} />
-			<Card title="Unhappy Potter 345" rating={9} />
-			<Card title="Simple Jack" rating={4} />
-			<Card title="Mission Possible" rating={4} />
-			<Card title="Super Easy Mission" rating={4} />
-		</div>
+		<>
+			<h1>This is the way!</h1>
+			<div className="card-container">
+				<Card title="Harry Fabricator 123" rating={8} isCool={true} />
+				<Card title="Unhappy Potter 345" rating={9} />
+				<Card title="Simple Jack" rating={4} />
+				<Card title="Mission Possible" rating={4} />
+				<Card title="Super Easy Mission" rating={4} />
+			</div>
+			<p class="light">This is light text.</p>
+			<p class="regular">This is regular text.</p>
+			<p class="medium">This is medium text.</p>
+			<p class="bold">This is bold text.</p>
+			<p class="extra-bold">This is extra bold text.</p>
+		</>
 	);
 };
 
